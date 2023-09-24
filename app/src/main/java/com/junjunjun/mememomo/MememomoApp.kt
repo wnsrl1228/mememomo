b940525ed6b31def2ee4e24eebb6a3f1d02d7c7e
wnsrl <wnsrl981228@gmail.com>
2023-09-24 14:52:12 +0900

    ui : appTopBar 적용

--- app/src/main/java/com/junjunjun/mememomo/MememomoApp.kt
+++ app/src/main/java/com/junjunjun/mememomo/MememomoApp.kt
@@ -1,13 +1,43 @@
 package com.junjunjun.mememomo
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.MoreVert
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.junjunjun.mememomo.component.MememomoTopAppBar
 import com.junjunjun.mememomo.navigation.MememomoNavHost
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MememomoApp(navController: NavHostController = rememberNavController()) {
     // TODO : 이 곳에서 배경, topbar, 테마, 초기 상태를 설정함
-    MememomoNavHost(navController = navController)
+    Scaffold() { padding ->
+        Row(
+            Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) {
+            // TODO : 추후 NavigationRail 추가
+            Column(
+                Modifier
+                    .fillMaxSize()
+                    .padding(padding)) {
+                MememomoTopAppBar(
+                    title = "메메모모",
+                    actionIcon = Icons.Rounded.MoreVert,
+                    actionIconContentDescription = ""
+                )
+                MememomoNavHost(navController = navController)
+            }
+        }
+    }
 }
 
